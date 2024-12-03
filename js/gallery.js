932ac8ab2d06ea2e674dd390abbdaf7977b9e954
Teszáry Péter <peterteszary@gmail.com>
2024-12-04 00:36:24 +0100

    Project Finished

--- js/gallery.js
+++ js/gallery.js
@@ -1,13 +1,25 @@
-$('.thumbnail').click(function(){
-	$('.modal-body').empty();
-	$($(this).parents('div').html()).appendTo('.modal-body');
-	$('#modal').modal({show:true});
+$(document).ready(function () {
+	// Modal megnyitása és kép dinamikus betöltése
+	$('.thumbnail a').click(function (event) {
+		event.preventDefault(); // Ne kövesse a linket
+		var imgSrc = $(this).find('img').attr('src'); // A kép forrásának kinyerése
+
+		// Modal body frissítése
+		$('.modal-body').empty(); // Töröljük a modal tartalmát
+		var modalContent = '<img src="' + imgSrc + '" class="img-fluid">'; // Kép hozzáadása
+		$('.modal-body').append(modalContent); // Modal body-ba való hozzáadás
+
+		// Modal megjelenítése
+		$('#modal').modal('show');
+	});
+
+	// Modal megjelenítésekor a képek elmosódása
+	$('#modal').on('show.bs.modal', function () {
+		$('.col-6, .row .thumbnail').addClass('blur');
+	});
+
+	// Modal eltüntetésekor a képek elmosódásának eltávolítása
+	$('#modal').on('hide.bs.modal', function () {
+		$('.col-6, .row .thumbnail').removeClass('blur');
+	});
 });
-
-$('#modal').on('show.bs.modal', function () {
-   $('.col-6,.row .thumbnail').addClass('blur');
-})
-
-$('#modal').on('hide.bs.modal', function () {
-   $('.col-6,.row .thumbnail').removeClass('blur');
-})
